b11e2446219c11ed8228cbeb1a1e8b086abdc6e7
shripatiavasthi <talu2frnds@gmail.com>
2021-08-11 18:51:02 +0530

    Backend inprogress

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+    }).then((con)=>{
+        console.log("Mongo DB connect to HOST :" , con.connection.host)
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+module.exports = connectDatabase
---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+const dotenv = require('dotenv');
+const connectDatabase = require('./config/database')
+
+dotenv.config({ path : './backend/config/config.env' })
+
+connectDatabase();
+
+app.listen(process.env.PORT, () => {
+    console.log(`server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+})

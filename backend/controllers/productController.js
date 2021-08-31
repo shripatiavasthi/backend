9d76392db7f163f5534d9151388f2c32a7178e80
shripatiavasthi <talu2frnds@gmail.com>
2021-08-31 13:14:05 +0530

    Last Commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,14 +1,70 @@
 const Product = require('../models/products')
 
-exports.newProduct = async(req,res,next) =>{
+exports.newProduct = async (req, res, next) => {
     const product = await Product.create(req.body)
-    res.status(201).json({ success:true, product })
+    res.status(201).json({ success: true, product })
 }
 
-exports.getProducts = (req,res,next) => {
+exports.getProducts = async (req, res, next) => {
+    const products = await Product.find();
     res.status(200).json({
-        success:true,
-        message :'This route will show all products'
+        success: true,
+        count: products.length,
+        message: 'This route will show all products',
+        products
     })
 }
 
+exports.getProductsByID = async (req, res, next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        res.status(404).json({
+            success: false,
+            message: 'No product with this id'
+        })
+    } else {
+        res.status(200).json({
+            success: true,
+            product
+        })
+    }
+}
+
+exports.delProductsByID = async (req, res, next) => {
+
+    const product = await Product.findByIdAndRemove(req.params.id);
+
+    if (!product) {
+        res.status(404).json({
+            success: false,
+            message: 'No product with this id'
+        })
+    } else {
+        res.status(200).json({
+            success: true,
+        })
+    }
+}
+
+exports.updateProductsByID = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product) {
+        res.status(404).json({
+            success: false,
+            message: 'No product with this id'
+        })
+    } 
+    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new : true,
+        runValidators : true,
+        useFindAndModify: false
+    })
+    res.status(200).json({
+        success :true,
+        product
+    })
+}
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router();
 
-const { getProducts,newProduct } = require('../controllers/productController')
+const {
+        getProductsByID,
+        getProducts,
+        newProduct,
+        delProductsByID,
+        updateProductsByID
+    } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
-router.route('/product/new').post(newProduct)
+router.route('/product/:id').get(getProductsByID);
+
+router.route('/admin/product/new').post(newProduct) //admin
+router.route('/admin/product/:id').delete(delProductsByID).put(updateProductsByID) //admin
 
 module.exports = router

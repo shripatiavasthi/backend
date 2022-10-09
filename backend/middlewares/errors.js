d089a160574cb54b0b49b717ae3871bdc8bf4b6a
shripati_avasthi <shripatiavasthi@redcliffelabs.com>
2022-10-09 16:26:49 +0530

    Added error stack

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -16,10 +16,24 @@ module.exports = (err,req,res,next) => {
     if(process.env.NODE_ENV === 'PRODUCTION'){
         let error = { ...err}
         error.message = err.message
+
+        // worng Mongoose object id
+        if(err.name == 'CastError'){
+            const message = `Resource not found. Invalid : ${err.path}`
+            error = new Error(message,400)
+        }
+
+        // Handling moongoose validation error
+        if(err.name == 'ValidationError'){
+            const message = Object.values(err.errors).map(value => value.message)
+            error = new ErrorHandler(message,400)
+        }
+
         res.status(error.statusCode).json({
             success : true,
             message : error.message || 'Internal Server Error',
         })
+
     }
 
     

ecddb80a7759082ae4a3f1a25c272d056baef595
shubham_tandon <shubham@phnk.in>
2023-04-05 20:00:38 +0530

    ADDED ROUTE

--- backend/routes/leadStatus.js
+++ backend/routes/leadStatus.js
@@ -9,9 +9,9 @@ const {
     updateLeadsStatusByID
     } = require('../controllers/leadStatusController')
 
-router.route('/category').get(getLeadsStatus);
-router.route('/category/:id').get(getLeadStatusByID);
-router.route('/admin/category/new').post(newLeadStatus) //admin
-router.route('/admin/category/:id').delete(delLeadStatusByID).put(updateLeadsStatusByID) //admin
+router.route('/leadstatus').get(getLeadsStatus);
+router.route('/leadstatus/:id').get(getLeadStatusByID);
+router.route('/admin/leadstatus/new').post(newLeadStatus) //admin
+router.route('/admin/leadstatus/:id').delete(delLeadStatusByID).put(updateLeadsStatusByID) //admin
 
 module.exports = router

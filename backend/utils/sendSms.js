c9201b75965a77ae9fbb544e7851b71bfa291851
Amit <amit@wyngatesolutions.com>
2023-05-28 00:48:40 +0530

    changes under prod

--- backend/utils/sendSms.js
+++ backend/utils/sendSms.js
@@ -2,15 +2,17 @@
 
 const sendSms = async options => {
     const client = require("twilio")(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-    await client.messages.create({ 
-        body: `${options.body}`,
-        from: process.env.TWILIO_PHONE_NUMBER, 
-        to: options.to 
-    }).then(() => {
-        console.log('SMS SENT')
-    }).catch(err => {
-        console.log(err,'SMS NOT SENT')
-    })
+    // if (process.env.NODE_ENV == "PRODUCTION") {
+        await client.messages.create({
+            body: `${options.body}`,
+            from: process.env.TWILIO_PHONE_NUMBER,
+            to: options.to
+        }).then(() => {
+            console.log('SMS SENT')
+        }).catch(err => {
+            console.log(err, 'SMS NOT SENT')
+        })
+    // }
 }
 
 module.exports = sendSms

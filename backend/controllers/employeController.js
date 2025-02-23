247c48c1fa4be66de5efab3454990d3e56f63373
verb-shripati <73355006+shripatiavasthi@users.noreply.github.com>
2025-02-23 14:54:20 +0530

    added error for file type missing

--- backend/controllers/employeController.js
+++ backend/controllers/employeController.js
@@ -3,18 +3,23 @@ const ErrorHandler = require('../utils/errorHandler');
 const catchAsynErrors = require('../middlewares/catchAsynErrors');
 
 exports.newEmployee = catchAsynErrors(async (req, res, next) => {
-
     
-    if(req.files["resume"][0].location){
+    if(req.files["resume"] && req.files["resume"][0].location){
     req.body.resume = req.files["resume"][0].location
+    }else{
+        return next(new ErrorHandler('Please provide file for resume', 400))
     }
     
-    if(req.files["diploma"][0].location){
+    if(req.files["diploma"] && req.files["diploma"][0].location){
     req.body.diploma = req.files["diploma"][0].location
+    }else{
+        return next(new ErrorHandler('Please provide file for diploma', 400))
     }
 
-    if(req.files["exp"][0].location){
+    if(req.files["exp"] && req.files["exp"][0].location){
     req.body.exp = req.files["exp"][0].location
+    }else{
+        return next(new ErrorHandler('Please provide file for exp', 400))
     }
     
     const employee = await Employee.create(req.body)
